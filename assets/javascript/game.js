84007fad42e394f97886e66be99e1ff0edd6e076
scottwagner <af17317596@gmail.com>
2018-10-04 15:23:06 -0500

    moved the Psuedocode around and started some Javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+// probably going to need some form of an if else statement at some point -->
+let words = ['Temple Guard', 'Chosen Warriors', 'White Lions', 'Demigryph Knights', 'Blood Knights', 'Ironbreakers'] 
+// something like "if correct display 'you are correct' else display 'you are wrong'" etc-->
+// and probably a for loop with the if else in to to end when blanks = zero -->
+// need a create an array of words to choose from-->
+// choose those words randomly -->
+// create some function that creates blanks equal to the length of selected word -->
+// check if guess is right -->
+// if right push to correct array -->
+// if wrong push to wrong array -->
